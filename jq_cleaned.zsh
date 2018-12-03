493aeb29825f3af32ead295891ef638d30c552aa
Indieterminacy <indieterminacy@nortonspeal.com>
2018-12-03 13:26:54 +0100

    Updated pathways and provided TODO items

--- jq_cleaned.zsh
+++ jq_cleaned.zsh
@@ -6,15 +6,17 @@
 ##############################
 ## nqn_pathways
 ##############################
-# TODO Migrate to 4q50nqn_pathways officually once all softlinks from earlier
-# redundant approach removed
-nqn_tq_pathways_aliases="$HOME/4q50nqn_pathways/oq_zsh/jq_configuring/tq_aliases.zsh"
-# nqn_tq_pathways_aliases="$HOME/4q_flow-qiuy/50_Environment/nqn_pathways/oq_zsh/jq_configuring/tq_aliases.zsh"
+# TODO Migrate to modern pathway
+local nen="$HOME/4q_flow-qiuy/50_Environment/nqn_pathways"
+local oq="zsh"
+local jq="configuring"
+local je="tq_aliases.zsh"
+local nqn_tq_pathways_aliases="$nen/oq_$oq/jq_$jq/$je"
 
 ##############################
 ## nq_package-managers
 ##############################
-Q5q60nq_oq_packagemanager_zsh="zplug"
+local Q5q60nq_oq_packagemanager_zsh="zplug"
 
 ##############################
 # iqi_initiating
@@ -26,6 +28,7 @@ if [[ -f $nqn_tq_pathways_aliases ]]
     then
         source $nqn_tq_pathways_aliases
     else
+        # TODO Introduce colour
         echo "WARNING: Pathways alias missing"
         echo "nqn_tq_pathways_aliases value: " $nqn_tq_pathways_aliases
 fi
@@ -38,13 +41,14 @@ compinit
 ## nq_package-managers
 ##############################
 
-if [[ -f $Q5q60iqi_initiating ]]; then
-    source $Q5q60iqi_initiating
+if [[ -f $oq_iei_zsh_initiating ]]; then
+    source $oq_iei_zsh_initiating
 else
     echo ""
+    # TODO Introduce colour
     echo "WARNING: Configuration file missing"
     echo "INFO: It is required for loading zsh package manager initiation steps"
-    echo "Q5q60iqi_initiating file value: $Q5q50iqi_initiating"
+    echo "oq_iei_zsh_initiating file value: $oq_iei_zsh_initiating"
 fi
 
 ####################
@@ -53,23 +57,23 @@ fi
 # jq_configuring
 ##############################
 function iqi_jq_oq_initiating_settings_zsh {
-    if [[ -d $QZSHSETTINGS ]]
+    if [[ -d $oq_je_zsh_settings ]]
         then
-            source $QZSHSETTINGS/jq_oqo_settings_plugins.zsh
-            source $QZSHSETTINGS/jq_oqo_settings_scripts.zsh
-            source $QZSHSETTINGS/jq_50_settings_environment.zsh
-            source $QZSHSETTINGS/jq_nqn_settings_aliases.zsh
-            source $QZSHSETTINGS/jq_iqi_settings_contexts.zsh
-            source $QZSHSETTINGS/jq_iqi_settings_bindings.zsh
+            source $oq_je_zsh_settings/jq_oqo_settings_plugins.zsh
+            source $oq_je_zsh_settings/jq_oqo_settings_scripts.zsh
+            source $oq_je_zsh_settings/jq_50_settings_environment.zsh
+            source $oq_je_zsh_settings/jq_nqn_settings_aliases.zsh
+            source $oq_je_zsh_settings/jq_iqi_settings_contexts.zsh
+            source $oq_je_zsh_settings/jq_iqi_settings_bindings.zsh
             # suggestion
             # https://stackoverflow.com/questions/2890651/zsh-for-loop-exclusion
             # set -o EXTENDED_GLOB
-            # for package in $QZSHSETTINGS/^os; do
+            # for package in $oq_je_zsh_settings/^os; do
             #     source ${package}
             # done
         else
             echo "ERROR: Directory missing for ZSH settings"
-            echo "QZSHSETTINGS pathway value: $QZSHSETTINGS"
+            echo "oq_je_zsh_settings pathway value: $oq_je_zsh_settings"
             echo ""
             echo "TIP: Consider investigating whether correct alias given in ZSH pathways configuration file"
             # TODO Build into function for loading variables file
@@ -110,4 +114,3 @@ fi
 # [ -f ~/6q_tools-qiuy/60_System/iqi_version-control/oq_lua/oqo_luaver/luaver/luaver/luaver.plugin.zsh ] && source ~/6q_tools-qiuy/60_System/iqi_version-control/oq_lua/oqo_luaver/luaver/luaver/luaver.plugin.zsh
 
 # [ -s ~/.luaver/luaver ] && . ~/.luaver/luaver
-
